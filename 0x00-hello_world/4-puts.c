91ea9319c762496e34b99a0004230af05570ca53
Ken Maganjo <kenniesons@gmail.com>
2022-09-08 08:40:37 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programing is like building a muiltilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"programing is like building a muiltilingual puzzle");
+	return (0);
+}
