be1222e297c01c589c02a50e3acb43e5b1e31367
Ken Maganjo <kenniesons@gmail.com>
2022-09-08 08:59:17 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Descriptions: using the main function
+ * this program prints "Programming is like building a muiltilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with a proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
