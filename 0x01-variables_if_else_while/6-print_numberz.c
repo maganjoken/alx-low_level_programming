f43366f7192b90a0bd27482d03f9cf334d6b1f1c
Ken Maganjo <kenniesons@gmail.com>
2022-09-10 07:24:10 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,55 +1,19 @@
-/* C program to print a long int number
- *
- *  using putchar() only*/
+#include<stdio.h> 
 
-#include <stdio.h>
+int main(){ 
 
- 
+	int num; 
 
-void print(long n)
+	scanf("%d",&num); 
 
-{
+	while(num){ 
 
-	    // If number is smaller than 0, put a - sign
-	    //
-	    //     // and change number to positive
-	    //
-	    //         if (n < 0) {
-	    //
-	    //                 putchar('-');
-	    //
-	    //                         n = -n;
-	    //
-	    //                             }
-	    //
-	    //                              
-	    //
-	    //                                  // Remove the last digit and recur
-	    //
-	    //                                      if (n/10)
-	    //
-	    //                                              print(n/10);
-	    //
-	    //                                               
-	    //
-	    //                                                   // Print the last digit
-	    //
-	    //                                                       putchar(n%10 + '0');
-	    //
-	    //                                                       }
-	    //
-	    //                                                        
-	    //
-	    //                                                        // Driver program to test above function
-	    //
-	    //                                                        int main()
-	    //
-	    //                                                        {
-	    //
-	    //                                                            long int n = 12045;
-	    //
-	    //                                                                print(n);
-	    //
-	    //                                                                    return 0;
-	    //
-	    //                                                                    }
+		  printf("%d\n",num%10); 
+
+		    num=num/10; 
+
+		     } 
+
+	return 0; 
+
+} 
