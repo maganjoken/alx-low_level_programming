98039f6b7d75cc97fe30ccd547b20253759dead9
Ken Maganjo <kenniesons@gmail.com>
2022-09-14 08:38:11 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,10 +7,9 @@ void times_table(void)
 {
 	int num, mult, prod;
 	
-	for (num = 0; num = 9; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
-
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
@@ -21,7 +20,6 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
-			
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
