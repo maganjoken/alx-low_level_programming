af19eaebe5e33ee14adfc5a8d3394721906c3bd6
Ken Maganjo <kenniesons@gmail.com>
2022-09-26 02:42:48 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+/**
+ * file: main.h
+ * Auth: maganjoken
+ * Description: header file containing prototypes
+ */
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+
+#endif
