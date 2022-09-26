bf4d4378f4aa0167c5d036ada796e057b5d17c5a
Ken Maganjo <kenniesons@gmail.com>
2022-09-26 07:13:59 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,24 +5,19 @@
  * @s: string
  * @c: character to search
  *
- * Return: pointer to the first occurrence of the character c in the strings
+ * Return: pointer to spot in s with c or null
  */
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0') /*Declaring WHILE*/
+	int i;
+
+	for (i =0; *(s + i); i++)
 	{
-		if (*s == c) /*if s == c*/
-		{
-			return (s); /*return s*/
-		}
-
-		++s;
+		if (*(s + i) == c)
+			return (s + i);
 	}
-	if (*s == c)
-	}
-		return (s);
-	}
-
-	return (0);/* values null*/
+	if (*(s + i) == c)
+		return (s + i);
+	return (0);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@
  * Auth: maganjoken
  * Description: header file containing prototypes
  */
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
