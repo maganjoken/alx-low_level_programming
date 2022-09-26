ea4a08aa1430a490d6bc077eaddfb359c4a5a2d9
Ken Maganjo <kenniesons@gmail.com>
2022-09-26 14:43:09 -0700

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
-  * _strpbrk - search a string for any of a set of bytes
-  * @s: source string
-  * @accept: accepted characters
+  * _strpbrk - searches a string for any of a set of bytes
+  * @s: The string to be searched
+  * @accept: The set of bytes to be searched for
   *
-  * Return: the string since the first found accepted character
+  * Return: If a set is matched - a pointer to the matched bytes
+  * 	    If no set is matched - NULL
   */
 char *_strpbrk(char *s, char *accept)
 {
