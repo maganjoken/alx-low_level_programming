c21657a36ea9e78e1d15d1190f407fef25897405
Ken Maganjo <kenniesons@gmail.com>
2022-09-26 09:46:45 -0700

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,15 +11,19 @@ char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
 	
-	for (i = 0; s[i]; i++
+	for (i = 0; *(s + i); i++)
 	{
-		for (j = 0; accept[j]; j++)
+		for (j = 0; *(accept + j); j++)
 		{
-			if (s[i] == accept[j])
+			if (*(s + i) == *(accept + j))
+			{
 				break;
+			}
 		}
-		if (accept[j])
+		if (*(accept + j) != '\0')
+		{
 			return (s + i);
+		}
 	}
 	return (0);
 }
