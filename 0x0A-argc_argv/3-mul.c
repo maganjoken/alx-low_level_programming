1de167cd04be184aacb46d798f0318d747edd94d
Ken Maganjo <kenniesons@gmail.com>
2022-09-30 06:14:20 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * main - prints the multipilication of two integers
  * @argc: counts arguments
  * @argv: argument vector
- * Return: 0 if true, 1 if false
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-	int a, b;
+	int n1 = 0; n2 = 0;
+
 	if (argc == 3)
 	{
-		a = ato1(argv[1]);
-		b = ato1(argv[2]);
-		printf("%d\n", a * b);
-		return (0);
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
 	}
+	else
+	{
 	printf("Error\n");
 	return (1);
+	}
+
+	return (0);
 }
