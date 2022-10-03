92a48f28af00e836ee081c2598763a600f3cc340
Ken Maganjo <kenniesons@gmail.com>
2022-10-03 08:52:24 -0700

    task 0

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+/**
+ * file: main.h
+ * Auth: maganjoken
+ * Description: header file containing prototypes
+ */
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+
+#endif
+
