aa1459e3ba95b53ec997fc3193547aacf3a299ad
Ken Maganjo <kenniesons@gmail.com>
2022-10-06 02:11:20 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: memory to allocate
+ * Return:pointer to allocated memory or normal process termination if error
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *q;
+
+	q = malloc(b);
+	if (q === NULL)
+		exit(98);
+	return (q);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+A readme file for C - More malloc, free files
