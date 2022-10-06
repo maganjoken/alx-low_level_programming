49bfffd9829370361725950a4d2930648e4aeb91
Ken Maganjo <kenniesons@gmail.com>
2022-10-06 04:48:39 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,21 +11,21 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	void *q;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(nmemb * size);
-	if (p == NULL)
+	q = malloc(nmemb * size);
+	if (q == NULL)
 	{
 		return (NULL);
 	}
 
 	for (i = 0; i < (nmemb * size); i++)
 	{
-		*((char *)(p) + i) = 0;
+		*((char *)(q) + i) = 0;
 	}
 
-	return (p)
+	return (q)
 }
