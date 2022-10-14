e6760effb50917596e52225a3d521274c6425c19
maganjoken <111058600+maganjoken@users.noreply.github.com>
2022-10-14 11:17:19 +0300

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,31 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include<stdio.h>
+#include<stdlib.h>
 /**
- * main - print opcodes of a given machine.
- * @argc: number of arguments.
- * @argv: argument vector.
- * return: 0
- */
+ * main- Entry point
+(* a blank line
+ *@argc: the number of parameters.
+ *@argv: the parameeters in the case the number ob bytes.
+* Description: this program prints opcodes in hexa)?
+* Return: 0 in succes
+*/
 int main(int argc, char *argv[])
 {
-	int count, bytes;
+	int i, n;
 
-	if(argc != 2)
+
+	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(1);
+		return (1);
 	}
-	bytes = atoi(argv[1]);
-	if(bytes < 0)
+	n = atoi(argv[1]);
+	if (n < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	for(count = 0; count < bytes; count++)
+
+	for (i = 0; i < n; i++)
 	{
-		printf("%02hhx", *((char *)main + count));
-		if(count < bytes - 1)
+		printf("%02hhx", *((char *)main + i));
+		if (i < n - 1)
 			printf(" ");
 		else
 			printf("\n");
