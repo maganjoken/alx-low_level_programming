56e1d1c53af00fc596b73f17ea9b5b3d0cf40303
maganjoken <111058600+maganjoken@users.noreply.github.com>
2022-10-31 20:59:21 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,18 +1,31 @@
-/*
- * File: 1-print_binary.c
- * Auth: Brennan D Baraban
- */
-
 #include "main.h"
-
+#include <unistd.h>
+#include <limits.h>
 /**
- * print_binary - Prints the binary representation of a number.
- * @n: The number to be printed in binary.
+ * print_binary - print an unsigned int in binary
+ *
+ * @n:
+ *
+ * Return: void
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
+	unsigned long int printbit = 1ul << 63;
+	char c = '0';
 
-	_putchar((n & 1) + '0');
+	while (!(printbit & n) && printbit != 0)
+		printbit = printbit >> 1;
+
+	if (printbit == 0)
+		write(1, &c, 1);
+
+	while (printbit)
+	{
+		if (printbit & n)
+			c = '1';
+		else
+			c = '0';
+		write(1, &c, 1);
+		printbit = printbit >> 1;
+	}
 }
