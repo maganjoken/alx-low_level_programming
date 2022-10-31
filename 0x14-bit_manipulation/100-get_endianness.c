faa457c94012f6df84e825a2f988d290479465a5
Ken Maganjo <kenniesons@gmail.com>
2022-10-31 12:09:35 -0700

    task 6

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,16 +1,1 @@
-#include "main.h"
-<<<<<<< HEAD
-
-=======
->>>>>>> 924a38376b31d6233849f78e20ec918e29bf45bd
-/**
- * get_endianness - return the endianness of the machine
- *
- * Return: 0 for big endian, 1 for little endian
- */
-int get_endianness(void)
-{
-	int n = 1;
-
-	return (*((char *) &n) + '0');
-}
+hgasggdssdg
