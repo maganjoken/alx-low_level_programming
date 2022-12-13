edf5e197005ede9cbc8ad3ac93967ee40f082fbb
Ken Maganjo <kenniesons@gmail.com>
2022-12-13 12:20:25 -0800

    task 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -2,8 +2,3 @@
 gcc -wall -wextra -werror -pedanttic -c -fPIC *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
-<<<<<<< HEAD
- 
-=======
-
->>>>>>> 559911cd57e5282e9578c1e05b9c53880bd2d772
